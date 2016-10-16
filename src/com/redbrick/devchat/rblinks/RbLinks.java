eae561352ccf7b2ef1ed642047bbb6449fc65a33
Dave K <dave@paddez.com>
2016-10-16 15:42:42 +0100

    Adding Blaze build rules

--- src/com/redbrick/devchat/rblinks/RbLinks.java
+++ src/com/redbrick/devchat/rblinks/RbLinks.java
@@ -2,11 +2,10 @@ package com.redbrick.devchat.rblinks;
 
 public class RbLinks{
     public static void main(String[] args) throws Exception{
-        
+
         RbLinkbot bot = new RbLinkbot();
         bot.setVerbose(true);
         bot.connect("irc.redbrick.dcu.ie");
-        bot.joinChannel("#rblinks");        
+        bot.joinChannel("#rblinks");
     }
 }
-    
