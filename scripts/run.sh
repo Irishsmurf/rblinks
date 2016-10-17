58ea42a8c987ccd1a8b7750e26320262bfc2dfbb
David K <dave@paddez.com>
2016-10-17 16:47:31 +0100

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 figlet "RbLinks - wafs"
 sleep 2
-COMPILE="bazel build ../src:rblinks"
+COMPILE="bazel build //src:rblinks"
 eval $COMPILE
-RUN="../bazel-bin/src/rblinks"
+RUN="bazel-bin/src/rblinks"
 eval $RUN
